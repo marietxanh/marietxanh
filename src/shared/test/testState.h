b914c860a8d2753c450ac3c4a9caa09aedfa5ed2
marietxanh <32092859+marietxanh@users.noreply.github.com>
2017-10-05 18:29:55 +0200

    ajout du code de test status

---
+++ src/shared/test/testState.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   test.h
+ * Author: joanna
+ *
+ * Created on 3 octobre 2017, 15:46
+ */
+
+#ifndef TESTSTATE_H
+#define TESTSTATE_H
+
+int testState();
+
+#endif /* TESTSTATE_H */
+
